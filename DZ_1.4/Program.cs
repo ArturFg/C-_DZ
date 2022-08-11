15196bbe75d5f837ff792191a3167986f6cf7ee9
Артур <110016503+ArturFg@users.noreply.github.com>
2022-08-11 21:42:59 +0300

    Задача 8

---
+++ DZ_1.4/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.Write("Введите целое число: ");
+int n = Convert.ToInt32(Console.ReadLine());
+int size = 0;
+//int end = n - 1;
+while (size < n - 1)
+{
+    if (n % 2 == 0)
+    {
+        Console.Write($"{n}; ");
+        n = n - 1;
+    }
+    else  
+
+    {
+        n = n - 1;
+        Console.Write($"{n}; ");
+        n = n - 1;
+    }
+}
