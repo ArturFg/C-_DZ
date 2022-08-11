01d815395b773c7b8639de5b04f866912beb06be
Артур <110016503+ArturFg@users.noreply.github.com>
2022-08-11 16:46:07 +0300

    Проверка на не чётность

---
+++ DZ_1.3/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите число чтобы проверить на чётность: ");
+int n = Convert.ToInt32(Console.ReadLine());
+
+if (n % 2 == 0)
+{
+    Console.WriteLine($"{n} - чётное.");
+}
+else
+{
+    Console.WriteLine($"{n} - нечётное.");
+}
