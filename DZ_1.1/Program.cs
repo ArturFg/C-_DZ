b4ea4b6f2b7b98ccc10f358013b409f38f424f1e
Артур <110016503+ArturFg@users.noreply.github.com>
2022-08-11 07:53:34 +0300

    Задача на нахождение максимального числа среди двух чисел

---
+++ DZ_1.1/Program.cs
@@ -0,0 +1,18 @@
+﻿/*git remote add origin https://github.com/ArturFg/C-_DZ.git
+git branch -M main
+git push -u origin main*/
+
+Console.Write("Введите первое число: ");
+int a = Convert.ToInt32(Console.ReadLine());
+
+Console.Write("Введите второе число: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+if (a > b)
+{
+    Console.WriteLine($"max = {a}");
+}
+else
+{
+    Console.WriteLine($"max = {b}");
+}
