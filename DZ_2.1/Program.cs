317d327867441e49f1000c0008ba366863a95ce5
Артур <110016503+ArturFg@users.noreply.github.com>
2022-08-16 16:41:44 +0300

    Урок2 задача10

---
+++ DZ_2.1/Program.cs
@@ -0,0 +1,24 @@
+﻿Console.WriteLine("Hello, World!");
+
+int ThreeNum(int num)
+{
+    while(num >= 999)
+    {
+        num = num / 10;
+    }
+    int three = num % 10;
+    return three;
+}
+
+Console.Write("Введите число трёхзначное и более: ");
+int number = Convert.ToInt32(Console.ReadLine());
+
+if(number <= 99)
+{
+    Console.Write("Ошибка! Третьей цыфры ненайдено. Напишите число в котором три и более цифры.");
+}
+else
+{
+    int con = ThreeNum(number);
+    Console.WriteLine($"Третья цыфра числа {number}: {con}.");
+}
