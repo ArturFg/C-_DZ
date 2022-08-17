1fd7931d4319895ef6d0e0dd8a8ba6a7cfe6e0f3
Артур <110016503+ArturFg@users.noreply.github.com>
2022-08-17 21:08:01 +0300

    Задача 23 -> кубы.

---
+++ DZ_3.1/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.WriteLine("Hello, World! Задача 23.");
+
+Console.Write("Введите число у которого хотите узнать квадраты: ");
+int ctwo = Convert.ToInt32(Console.ReadLine());
+
+int schet = ctwo + 1;
+int index = 1;
+
+while(index < schet)
+{
+    int number = index * index * index;
+    Console.WriteLine(number);
+    index++;
+}
+
