e12e3229d4b2b3426d41fba584bfd13c4caf355e
Артур <110016503+ArturFg@users.noreply.github.com>
2022-08-18 08:45:30 +0300

    Урок 3 задача 19 -> палиандр

---
+++ DZ_3.3/Program.cs
@@ -0,0 +1,19 @@
+﻿Console.WriteLine("Hello, World!  Задача 19.");
+
+Console.Write("Введите пятизначное число что бы понять полидр ли это: ");
+int polydrome = Convert.ToInt32(Console.ReadLine());
+
+int divisionMax = polydrome / 10000;
+int remainderMin = polydrome % 10;
+
+int remainderMax = polydrome % 10000;
+int divisionMin = remainderMax / 10;
+
+int divisionAverage = divisionMin / 100;
+int remainder = divisionMin % 10;
+
+if(divisionMax == remainderMin && divisionAverage == remainder) Console.Write($"Число {polydrome} являеться полидром.");
+else Console.Write("Это число не полидром. Спроси у гугла: полиндр это?");
+
+
+
