798bd76ae936964fc7d0b9792f8734bda626811e
Артур <110016503+ArturFg@users.noreply.github.com>
2022-08-22 23:06:09 +0300

    Сименар 4, задача 27

---
+++ DZ_4.2/Program.cs
@@ -0,0 +1,33 @@
+﻿Console.WriteLine("Hello, World! Задача 27.");
+
+int SizeVon(int number)
+{
+    int si = 1;
+    while(number >= 9)
+    {
+        number /= 10;
+        si++;
+    }
+    return si;
+}
+int SumNum(int nu,int check)
+{
+    int sum = 0;
+    for (int i = 0; i < check; i++)
+    {
+        int preserva = nu;
+        int division = preserva / 10;
+        int remainder = nu % 10;
+        sum = sum + remainder;
+        nu = division;
+    }
+    return sum;
+}
+
+Console.Write("Введите число чтобы определить сумму его цыфр: ");
+int num = Convert.ToInt32(Console.ReadLine());
+
+int size = SizeVon(num);
+int answer = SumNum(num, size);
+
+Console.WriteLine($"Сумма цыфр числа {num} = {answer}.");
