d41f3d459fccaae6c2ba8ad3119c7bbad58df748
Артур <110016503+ArturFg@users.noreply.github.com>
2022-08-22 23:04:32 +0300

    Сименар 4, задача 25.

---
+++ DZ_4.1/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Hello, World! Задача 25.");
+
+int PereMultiply(int o, int d)
+{
+    int num = o;
+    for (int i = 1; i < d; i++)
+    {
+    num = num * o;
+    }
+    return num;
+}
+
+Console.Write("Введите число A, которое хотите пере множать: ");
+int a = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите число B, для того чтобы определить сколько раз перемножать число A: ");
+int b = Convert.ToInt32(Console.ReadLine());
+
+int answer = PereMultiply(a, b);
+
+Console.WriteLine($"Число {a} в степени {b} = {answer}.");
